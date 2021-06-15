5d74316c91e3308ac653844d955d0a081405b70c
phoomin009 <phoomin009@gmail.com>
2021-06-15 15:43:23 +0700

    Add auth and staff route

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import './module/mongoose.js'
-import { startServer } from './module/server.js'
+import { app, startServer } from './module/server.js'
+
+import staffRoute from './route/staff.js'
+import authRoute from './route/auth.js'
 
 // Express Middleware
 
 // Socket.io Middleware
 
 // Routes
+app.use('/api', authRoute)
+app.use('/api', staffRoute)
 
 startServer()
