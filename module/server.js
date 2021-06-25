8599da868e69025aa28b46e2ad82ff5e48fcf760
phoomin009 <phoomin009@gmail.com>
2021-06-25 12:37:25 +0700

    Fix server body parse json

--- module/server.js
+++ module/server.js
@@ -2,7 +2,10 @@ import http from 'http'
 import Express from 'express'
 import { Server } from 'socket.io'
 
+process.env.PORT = process.env.PORT || 3001
+
 export const app = Express()
+app.use(Express.json())
 
 const httpServer = http.createServer(app)
 
@@ -12,6 +15,6 @@ export const io = new Server(httpServer, {
 
 export function startServer () {
   httpServer.listen(process.env.PORT, () => {
-    console.log('Start server')
+    console.log('Start server 0.0.0.0:' + process.env.PORT)
   })
 }
