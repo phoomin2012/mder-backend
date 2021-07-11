6821d98bd730c22c01e62ab43fe2c940974cb367
phoomin009 <phoomin009@gmail.com>
2021-07-11 23:54:22 +0700

    Add update patient

--- route/patient.js
+++ route/patient.js
@@ -11,24 +11,24 @@ route.post('/patient', async function CreateOrUpdatePatient (req, res) {
   const formErrors = []
   if (!req.body.hospitalNumber) {
     formErrors.push('hospitalNumber.empty')
-  } else if (validator.isEmpty(req.body.hospitalNumber.toString())) {
+  } else if (validator.isEmpty(req.body.hospitalNumber.toString().trim())) {
     formErrors.push('hospitalNumber.empty')
   }
 
   if (req.body.bedNumber) {
-    if (validator.isEmpty(req.body.bedNumber)) {
+    if (validator.isEmpty(req.body.bedNumber.trim())) {
       formErrors.push('bedNumber.empty')
     }
   }
 
   if (req.body.name) {
-    if (validator.isEmpty(req.body.name)) {
+    if (validator.isEmpty(req.body.name.trim())) {
       formErrors.push('name.empty')
     }
   }
 
   if (req.body.lastName) {
-    if (validator.isEmpty(req.body.lastName)) {
+    if (validator.isEmpty(req.body.lastName.trim())) {
       formErrors.push('lastName.empty')
     }
   }
@@ -71,6 +71,36 @@ route.post('/patient', async function CreateOrUpdatePatient (req, res) {
 
   if (req.body.id) {
     // Update patient
+    const entry = parse(`${req.body.entryDate} ${req.body.entryTime}`, 'yyyy-MM-dd HH:mm:ss', new Date())
+    const exit = parse(`${req.body.exitDate} ${req.body.exitTime}`, 'yyyy-MM-dd HH:mm:ss', new Date())
+
+    const patient = await Patient.findById(req.body.id).exec()
+    patient.hospitalNumber = req.body.hospitalNumber
+    patient.bedNumber = req.body.bedNumber
+    patient.name = req.body.name
+    patient.lastName = req.body.lastName
+    patient.triage = req.body.triage
+
+    if (req.body.stage.toString() !== patient.currentStage.toString()) {
+      patient.currentStage = req.body.stage
+      patient.stages[patient.stages.length - 1].end = new Date()
+      patient.stages.push({
+        stage: req.body.stage,
+        start: new Date(),
+        end: null,
+      })
+    }
+
+    patient.entry = entry
+    patient.exit = isNaN(exit) ? null : exit
+    await patient.save()
+
+    // Broadcast patient information
+    io.emit('patient.update', patient)
+
+    return res.json({
+      success: true,
+    })
   } else {
     // New patient
     const entry = parse(`${req.body.entryDate} ${req.body.entryTime}`, 'yyyy-MM-dd HH:mm:ss', new Date())
@@ -81,7 +111,6 @@ route.post('/patient', async function CreateOrUpdatePatient (req, res) {
       bedNumber: req.body.bedNumber,
       name: req.body.name,
       lastName: req.body.lastName,
-      ventilator: req.body.ventilator,
       triage: req.body.triage,
       currentStage: req.body.stage,
       stages: stages,
@@ -98,7 +127,19 @@ route.post('/patient', async function CreateOrUpdatePatient (req, res) {
 })
 
 route.delete('/patient/:id', async function RemovePatient (req, res) {
+  const patient = await Patient.findById(req.params.id).exec()
 
+  if (patient) {
+    try {
+      await patient.remove()
+      io.emit('patient.remove', req.params.id)
+      return res.status(200).json({ success: true })
+    } catch (err) {
+      return res.status(400).json({ success: false })
+    }
+  } else {
+    return res.status(400).json({ success: false })
+  }
 })
 
 export default route
