085202cc9d4c43dfdde81218d07cbb7bda6fa2fb
phoomin2012 <phoomin009@gmail.com>
2021-09-07 13:45:10 +0700

    เพิ่มเก็บข้อมูล chart 1,2,4

--- task/dataCollector.js
+++ task/dataCollector.js
@@ -1,14 +1,76 @@
 import { CronJob } from 'cron'
+import { differenceInSeconds } from 'date-fns'
 import { Point } from '@influxdata/influxdb-client'
 import client, { org, bucket } from '../module/influx.js'
-import statisticModel from '../model/statistic.js'
+import StatisticModel from '../model/statistic.js'
+import PatientModel from '../model/patient.js'
 
 const job = new CronJob('*/5 * * * * *', async () => {
+  const __start = new Date()
   console.log('🤖 Start collect data to InfluxDB 📈📉')
   const writeApi = client.getWriteApi(org, bucket)
-  const statistic = await statisticModel.findOne({})
-  // console.log(statistic)
 
+  // Chart 1 => patient triage level
+  const patients = await PatientModel.find()
+  if (patients) {
+    const levels = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+    }
+
+    for (const patient of patients) {
+      levels[patient.triage] += 1
+    }
+
+    const point = new Point('triage')
+    point.intField('1', levels[1])
+    point.intField('2', levels[2])
+    point.intField('3', levels[3])
+    point.intField('4', levels[4])
+    point.intField('5', levels[5])
+
+    writeApi.writePoint(point)
+  }
+
+  // Chart 2 => average time interval
+  if (patients) {
+    const people = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+    }
+    const times = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+    }
+
+    for (const patient of patients) {
+      const lastStage = patient.stages[patient.stages.length - 1]
+
+      people[patient.triage] += 1
+      times[patient.triage] += differenceInSeconds(lastStage.start, new Date())
+    }
+
+    const point = new Point('timeInterval')
+    point.intField('1', people[1] > 0 ? times[1] / people[1] : 0)
+    point.intField('2', people[2] > 0 ? times[2] / people[2] : 0)
+    point.intField('3', people[3] > 0 ? times[3] / people[3] : 0)
+    point.intField('4', people[4] > 0 ? times[4] / people[4] : 0)
+    point.intField('5', people[5] > 0 ? times[5] / people[5] : 0)
+
+    writeApi.writePoint(point)
+  }
+
+  // Chart 3 => amount of staff and patient
+  const statistic = await StatisticModel.findOne({})
   if (statistic) {
     const point = new Point('population')
     point.intField('physician', statistic.currentPhysician)
@@ -17,11 +79,33 @@ const job = new CronJob('*/5 * * * * *', async () => {
 
     writeApi.writePoint(point)
   }
+
+  // Chart 4 => average length of stay
+  if (patients) {
+    let peoples = 0
+    let times = 0
+
+    for (const patient of patients) {
+      peoples += 1
+      times += differenceInSeconds(patient.entry, new Date())
+    }
+
+    const point = new Point('timeStay')
+    point.intField('all', times / peoples)
+
+    writeApi.writePoint(point)
+  }
+
+  // Chart 5 => overcrowding score
+  if (patients && statistic) {
+    // ....
+  }
+
   writeApi.close().then(() => {
-    console.log('\tFINISH')
+    console.log(`\tFINISH in ${Date.now() - __start.getTime()}ms`)
   }).catch(e => {
-    console.error(e)
     console.log('\nFinished ERROR')
+    console.error(e)
   })
 }, null, true, 'Asia/Bangkok')
 
