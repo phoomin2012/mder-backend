d4f90104ed2d3d55d695f839091c49e50dda5166
phoomin2012 <phoomin009@gmail.com>
2021-09-07 14:19:58 +0700

    API ดึงข้อมูลกราฟ 1,2,4

--- route/history.js
+++ route/history.js
@@ -9,16 +9,86 @@ route.get('/history', jwtMiddleware, async (req, res) => {
   const queryApi = client.getQueryApi(org)
   const jsonResponse = {}
 
-  const query = `from(bucket: "${bucket}")
-  |> range(start: -1h)
-  |> filter(fn: (r) => r["_measurement"] == "population")
-  |> aggregateWindow(every: 5s, fn: mean, createEmpty: false)
-  |> yield(name: "mean")
-  `
-
   const _start = Date.now()
 
+  // Chart 1 => patient triage level
   try {
+    const query = `from(bucket: "${bucket}")
+      |> range(start: -1h)
+      |> filter(fn: (r) => r["_measurement"] == "triage")
+      |> aggregateWindow(every: 5s, fn: mean, createEmpty: false)
+      |> yield(name: "mean")
+      `
+    const rawData = await queryApi.collectRows(query)
+    console.log('\nCollect ROWS SUCCESS')
+
+    const mappedData = {}
+    for (const row of rawData) {
+      const time = getTime(parseJSON(row._time))
+      if (typeof mappedData[time] === 'undefined') {
+        mappedData[time] = {
+          time: time,
+          triage: {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+          },
+        }
+      }
+      mappedData[time].triage[row._field] = row._value
+    }
+
+    jsonResponse.chart1 = Object.values(mappedData)
+  } catch (error) {
+    console.error(error)
+    console.log('\nCollect ROWS ERROR')
+  }
+
+  // Chart 2 => average time interval
+  try {
+    const query = `from(bucket: "${bucket}")
+      |> range(start: -1h)
+      |> filter(fn: (r) => r["_measurement"] == "timeInterval")
+      |> aggregateWindow(every: 5s, fn: mean, createEmpty: false)
+      |> yield(name: "mean")
+      `
+    const rawData = await queryApi.collectRows(query)
+    console.log('\nCollect ROWS SUCCESS')
+
+    const mappedData = {}
+    for (const row of rawData) {
+      const time = getTime(parseJSON(row._time))
+      if (typeof mappedData[time] === 'undefined') {
+        mappedData[time] = {
+          time: time,
+          triage: {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+          },
+        }
+      }
+      mappedData[time].triage[row._field] = row._value
+    }
+
+    jsonResponse.chart2 = Object.values(mappedData)
+  } catch (error) {
+    console.error(error)
+    console.log('\nCollect ROWS ERROR')
+  }
+
+  // Chart 3 => amount of staff and patient
+  try {
+    const query = `from(bucket: "${bucket}")
+      |> range(start: -1h)
+      |> filter(fn: (r) => r["_measurement"] == "population")
+      |> aggregateWindow(every: 5s, fn: mean, createEmpty: false)
+      |> yield(name: "mean")
+      `
     const rawData = await queryApi.collectRows(query)
     console.log('\nCollect ROWS SUCCESS')
 
@@ -42,8 +112,38 @@ route.get('/history', jwtMiddleware, async (req, res) => {
     console.log('\nCollect ROWS ERROR')
   }
 
-  console.log(`⏱ Fetch history in ${Date.now() - _start}ms.`)
+  // Chart 4 => average length of stay
+  try {
+    const query = `from(bucket: "${bucket}")
+    |> range(start: -1h)
+    |> filter(fn: (r) => r["_measurement"] == "timeStay")
+    |> aggregateWindow(every: 5s, fn: mean, createEmpty: false)
+    |> yield(name: "mean")
+    `
+    const rawData = await queryApi.collectRows(query)
+    console.log('\nCollect ROWS SUCCESS')
 
+    const mappedData = {}
+    for (const row of rawData) {
+      const time = getTime(parseJSON(row._time))
+      if (typeof mappedData[time] === 'undefined') {
+        mappedData[time] = {
+          time: time,
+          all: 0,
+        }
+      }
+      mappedData[time][row._field] = row._value
+    }
+
+    jsonResponse.chart4 = Object.values(mappedData)
+  } catch (error) {
+    console.error(error)
+    console.log('\nCollect ROWS ERROR')
+  }
+
+  // Chart 5 => overcrowding score
+
+  jsonResponse.finishIn = Date.now() - _start
   return res.json(jsonResponse)
 })
 
